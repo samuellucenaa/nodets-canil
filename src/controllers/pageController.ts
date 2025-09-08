9ce6fb59e29e2f75bfb96c55631abbf9c589fc8b
samuellucenaa <samuellucenaa1@gmail.com>
2025-09-08 16:26:47 -0300

    sda

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,37 @@
 import type { Request, Response } from "express";
 
 export const home = (req: Request, res: Response)=>{
-    res.render('pages/page')
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os animais',
+            background: '/public/images/allanimails.jpg'
+        }
+    })
 }
 
 export const cachorros = (req: Request, res: Response)=>{
-    res.send('Cachorros')
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os cachorros',
+            background: '/public/images/banner_dog.jpg'
+        }
+    })
 }
 
 export const gatos = (req: Request, res: Response)=>{
-    res.send('Gatos')
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os gatos',
+            background: '/public/images/cat.jpg'
+        }
+    })
 }
 
 export const peixes = (req: Request, res: Response)=>{
-    res.send('Peixes')
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os peixes',
+            background: '/public/images/fish.jpg'
+        }
+    })
 }
